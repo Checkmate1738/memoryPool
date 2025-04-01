ade72c879ff56f91377a3c99af75aad79cfb88d5
Checkmate1738 <143074247+Checkmate1738@users.noreply.github.com>
2025-04-01 19:06:41 +0300

    e

--- memapp/src/content/auth/Login.jsx
+++ memapp/src/content/auth/Login.jsx
@@ -2,8 +2,10 @@ import { NavHeader, SmallDeviceNav, isSmallDevice } from "@src/App";
 import { Link as Route, useNavigate } from "react-router-dom";
 import { Box, Button, HStack, Link } from "@chakra-ui/react";
 import { LuChevronLeft } from "react-icons/lu";
+import { useColorModeValue } from "@src/components/ui/color-mode";
 
 function LoginHeader() {
+  const backBg = useColorModeValue("blue.200","blue.400")
   const navigate = useNavigate();
   const isSmall = isSmallDevice();
 
@@ -29,7 +31,8 @@ function LoginHeader() {
           ]}
         />
       ) : (
-        <Button type="button" variant={"outline"} onClick={back}>
+        <Button type="button" variant={"outline"} onClick={back} bgColor={backBg} display={"flex"} alignItems={"center"}>
+          <LuChevronLeft/>
           Back
         </Button>
       )}
