b59e15a6f9146964f1e84d2715a78ae70b91519a
Checkmate1738 <brianmureithi64@gmail.com>
2025-04-02 16:49:17 +0300

    modified:   memapp/src/content/auth/Register.jsx

--- memapp/src/content/auth/Register.jsx
+++ memapp/src/content/auth/Register.jsx
@@ -1,49 +1,190 @@
-import { NavHeader,isSmallDevice,SmallDeviceNav } from "@src/App"
-import { Link as Route,useNavigate} from "react-router-dom"
-import { Box, Button, HStack, Link } from "@chakra-ui/react"
-import {useColorModeValue} from "@src/components/ui/color-mode"
-import { LuChevronLeft } from "react-icons/lu"
+import { NavHeader, isSmallDevice, SmallDeviceNav } from "@src/App";
+import { Link as Route, useNavigate } from "react-router-dom";
+import {
+  Box,
+  Button,
+  HStack,
+  Link,
+  VStack,
+  Field,
+  Input,
+  InputGroup,
+} from "@chakra-ui/react";
+import { useColorModeValue } from "@src/components/ui/color-mode";
+import { useForm } from "react-hook-form";
+import { FiEdit3 } from "react-icons/fi";
+import { TbLockPassword } from "react-icons/tb";
+import { MdOutlineMail } from "react-icons/md";
+import { LuChevronLeft } from "react-icons/lu";
+import axios from "axios";
 
 function RegisterHeader() {
-    const backBg = useColorModeValue("blue.200","blue.400")
-    const backColor = useColorModeValue("blue.500","blue.800")
-    const navigate = useNavigate()
-    const small = isSmallDevice()
+  const backBg = useColorModeValue("blue.200", "blue.400");
+  const backColor = useColorModeValue("blue.500", "blue.800");
+  const navigate = useNavigate();
+  const small = isSmallDevice();
 
-    const back = () => {
-        navigate(-1)
+  const back = () => {
+    navigate(-1);
+  };
+
+  return small ? (
+    <SmallDeviceNav
+      data={[
+        <Box
+          width={"100%"}
+          onClick={back}
+          display={"flex"}
+          padding={2}
+          alignItems={"center"}
+        >
+          <LuChevronLeft />
+          Back
+        </Box>,
+      ]}
+    />
+  ) : (
+    <HStack>
+      <Button
+        type="button"
+        variant={"subtle"}
+        borderRadius={12}
+        size={"lg"}
+        onClick={back}
+        bgColor={backBg}
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"flex-start"}
+        color={backColor}
+      >
+        <LuChevronLeft />
+        Back
+      </Button>
+    </HStack>
+  );
+}
+
+function RegisterModel() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
+  const navigate = useNavigate();
+
+  const submit = async (data) => {
+    if (data.password !== data.confirmPassword) {
+      alert("Passwords don't match");
+    }
+    delete data.confirmPassword;
+    data["role"] = "member";
+    data["sender"] = "browser";
+    console.log(data);
+    const url = "http://192.168.100.56:3560/auth/register";
+    const header = {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    };
+    try {
+      const feedback = await axios.post(url,data,{headers: header});
+      console.log(feedback.data)
+      if (feedback.data.message === "success") {
+        navigate("/auth/login")
+      }
+    } catch (error) {
+      console.log(error);
     }
 
-    return small ? (
-      <SmallDeviceNav
-        data={[
-          <Box
-            width={"100%"}
-            onClick={back}
-            display={"flex"}
-            padding={2}
-            alignItems={"center"}
-          >
-            <LuChevronLeft />
-            Back
-          </Box>,
-        ]}
-      />
-    ) : (
-      <HStack>
-        <Button type="button" variant={"subtle"} borderRadius={12} size={"lg"} onClick={back} bgColor={backBg} display={"flex"} alignItems={"center"} justifyContent={"flex-start"} color={backColor}>
-          <LuChevronLeft/>
-          Back
-        </Button>
-      </HStack>
-    );
+    reset();
+  };
+
+  return (
+    <VStack width={"max-content"}>
+      <form
+        onSubmit={handleSubmit((e) => {
+          submit(e);
+        })}
+      >
+        <VStack gapY={2}>
+          <Field.Root required invalid={!!errors.fullname}>
+            <InputGroup startElement={<FiEdit3 />}>
+              <Input
+                type={"text"}
+                {...register("fullName", { require: true })}
+                placeholder="John Doe"
+              />
+            </InputGroup>
+            <Field.ErrorText>{errors.fullname}</Field.ErrorText>
+          </Field.Root>
+          <Field.Root required invalid={!!errors.username}>
+            <InputGroup startElement={<FiEdit3 />}>
+              <Input
+                type={"text"}
+                {...register("userName", { require: true })}
+                placeholder="Doe123"
+              />
+            </InputGroup>
+            <Field.ErrorText>{errors.username}</Field.ErrorText>
+          </Field.Root>
+          <Field.Root required invalid={!!errors.email}>
+            <InputGroup startElement={<MdOutlineMail />}>
+              <Input
+                type={"email"}
+                {...register("email", { require: true })}
+                placeholder="johndoe@example.com"
+              />
+            </InputGroup>
+            <Field.ErrorText>{errors.email}</Field.ErrorText>
+          </Field.Root>
+          <Field.Root required invalid={!!errors.password}>
+            <InputGroup startElement={<TbLockPassword />}>
+              <Input
+                type="password"
+                {...register("password", { require: true })}
+                placeholder="password"
+              />
+            </InputGroup>
+            <Field.ErrorText>{errors.password}</Field.ErrorText>
+          </Field.Root>
+          <Field.Root required invalid={!!errors.confirmPassword}>
+            <InputGroup startElement={<TbLockPassword />}>
+              <Input
+                type="password"
+                {...register("confirmPassword", { require: true })}
+                placeholder="confirmPassword"
+              />
+            </InputGroup>
+            <Field.ErrorText>{errors.confirmPassword}</Field.ErrorText>
+          </Field.Root>
+          <Input
+            type="submit"
+            bgColor={"colorPalette.400"}
+            textAlign={"center"}
+            value={"REGISTER"}
+          />
+        </VStack>
+      </form>
+      <Link as={Route} to="/auth/login">
+        Already have an account
+      </Link>
+    </VStack>
+  );
 }
 
 export default function Register() {
-    return <>
-    <NavHeader nav={<RegisterHeader/>}/>
-    <Box position={"absolute"} top={24} width={"100vw"}>
-        {"Register"}
-    </Box>
+  return (
+    <>
+      <NavHeader nav={<RegisterHeader />} />
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        height={"100vh"}
+        width={"100vw"}
+      >
+        <RegisterModel />
+      </Box>
     </>
+  );
 }
