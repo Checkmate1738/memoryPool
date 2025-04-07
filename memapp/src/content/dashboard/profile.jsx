db3eeb6da56c5bcfb1d6de29e19b6cdae738b1ae
Checkmate1738 <brianmureithi64@gmail.com>
2025-04-07 12:03:22 +0300

    modified:   memapp/src/content/dashboard/profile.jsx 	modified:   memapp/vite.config.js

--- memapp/src/content/dashboard/profile.jsx
+++ memapp/src/content/dashboard/profile.jsx
@@ -1,8 +1,19 @@
-import { useNavigate,Link as Route } from "react-router-dom";
-import { isSmallDevice,SmallDeviceNav,NavHeader } from "@src/App";
-import { Box,HStack,Link } from "@chakra-ui/react";
+import { useNavigate, Link as Route } from "react-router-dom";
+import { useState } from "react";
+import { isSmallDevice, SmallDeviceNav, NavHeader } from "@src/App";
+import {
+  Box,
+  Field,
+  HStack,
+  Input,
+  InputGroup,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
-function ProfileLink () {
+function ProfileLink() {
   const small = isSmallDevice();
   const navigate = useNavigate();
 
@@ -59,10 +70,101 @@ function ProfileLink () {
   );
 }
 
+function ProfileModel() {
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [isEdit, setIsEdit] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const [profileData, setProfileData] = useState({
+    fullname: "John Doe",
+    username: "Doe123",
+    email: "johndoe@example.com",
+    password: "*******",
+    tasks: 5,
+    notes: 2,
+  });
+
+  return (
+    <form
+      onSubmit={handleSubmit((e) => {
+        console.log(e);
+        setProfileData(e);
+        setIsDisabled(true);
+        setIsEdit(false);
+      })}
+    >
+      <VStack width={72}>
+        {Object.entries(profileData).map(([key, value]) => {
+          if (key === "tasks" || key === "notes") {
+            return (
+              <Field.Root key={key}>
+                <InputGroup startAddon={key}>
+                  <Input textAlign={"center"} value={value} readOnly />
+                </InputGroup>
+              </Field.Root>
+            );
+          }
+          return (
+            <Field.Root key={key}>
+              <InputGroup startAddon={key}>
+                {isDisabled ? (
+                  <Input value={value} readOnly />
+                ) : (
+                  <Input
+                    placeholder={value} {...register(key, { required: true })}
+                  />
+                )}
+              </InputGroup>
+            </Field.Root>
+          );
+        })}
+        <HStack width={"inherit"}>
+          {isEdit ? (
+            <Input
+              value={"Submit"}
+              type={"submit"}
+              cursor={"pointer"}
+              textAlign={"center"}
+            />
+          ) : (
+            <Input
+              value={"Edit"}
+              type={"button"}
+              onClick={() => {
+                setIsDisabled(false);
+                setIsEdit(true);
+              }}
+              cursor={"pointer"}
+              textAlign={"center"}
+            />
+          )}
+          <Input
+            type="button"
+            textAlign={"center"}
+            cursor={"pointer"}
+            value={"Log out"}
+            onClick={() => {
+              console.log("logged out");
+            }}
+          />
+        </HStack>
+      </VStack>
+    </form>
+  );
+}
+
 export default function Profile() {
-    return (
-        <>
-        <NavHeader nav={<ProfileLink/>}/>
-        </>
-    )
+  return (
+    <>
+      <NavHeader nav={<ProfileLink />} />
+      <Box
+        minHeight={"100vh"}
+        width={"100vw"}
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <ProfileModel />
+      </Box>
+    </>
+  );
 }
